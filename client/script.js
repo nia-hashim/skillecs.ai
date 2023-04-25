cb88ca54536698541e006ce398bda94d7b93581d
nia-hashim <129951091+nia-hashim@users.noreply.github.com>
2023-04-25 10:22:52 +0530

    commit new changes

--- client/script.js
+++ client/script.js
@@ -1,7 +1,5 @@
 import bot from './assets/bot.svg'
 import user from './assets/user.svg'
-import { Analytics } from "@vercel/analytics"
-
 
 const form = document.querySelector('form')
 const chatContainer = document.querySelector('#chat_container')
@@ -108,7 +106,6 @@ const handleSubmit = async (e) => {
         messageDiv.innerHTML = "Something went wrong"
         alert(err)
     }
-
 }
 
 form.addEventListener('submit', handleSubmit)
